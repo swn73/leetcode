6b159afd74930fa2c8d789ac0a9b9cc473003976
hualin.xhl <hualin.xhl@alibaba-inc.com>
2018-10-24 20:32:31 +0800

    addd

--- 0007_reverse-intege.cpp
+++ 0007_reverse-intege.cpp
@@ -10,32 +10,25 @@ using namespace std;
  * 
  */
 
-
-
-
-
 class Solution {
 public:
     int reverse(int x) {
         bool nagative=x<0?true:false; 
         x=x<0?-x:x;
-        string s;
-        long long n=x;
-        while(n){s+=('0'+n%10);n/=10;}
-        cout<<s<<endl;
-        n=0;
-        for(auto i=std::begin(s);i<std::end(s);++i){
+        int n=0;
+        while(x){
+            if((n*10)/10!=n){
+                return 0;
+            }
             n*=10;
-            cout<<n<<"\t"<<*i<<"\t"<<*i-'0'<<endl;
-            n+=(*i-'0'); 
-            cout<<n<<endl;
+            n+=(x%10);
+            x/=10;
         }
-        cout<<n<<endl;
         return nagative?-n:n;
     }
-};
+ };
 int main(){
     Solution s;
-    // assert(321==s.reverse(123));
+    assert(321==s.reverse(123));
     assert(321==s.reverse(1534236469));
 }
